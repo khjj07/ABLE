52be4b16a059bb702fd198cbe22898de99f6f4df
kruskal <yonghobae12@naver.com>
2021-12-09 15:10:37 +0900

    EndPoint UI Invoke

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EndPoint : MonoBehaviour
 {
+
+    public UnityEvent UI;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,7 @@ public class EndPoint : MonoBehaviour
         {
             Destroy(gameObject, 1f);
             Debug.Log("Game Clear");
-
+            UI.Invoke();
         }
     }
 
